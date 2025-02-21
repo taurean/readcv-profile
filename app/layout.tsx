e43fe708a8e1fc3bfef6f9a3e16b9a45efb7503f
Taurean Bryant <later@taurean.work>
2025-02-21 11:14:55 -0800

    attempting to change the OG image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,37 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google'
-import { promises as fs } from 'fs';
+import { Inter } from "next/font/google";
+import { promises as fs } from "fs";
 import "./globals.css";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-})
+    subsets: ["latin"],
+    variable: "--font-inter",
+});
 
 export async function generateMetadata(): Promise<Metadata> {
-  const file = await fs.readFile(process.cwd() + '/public/content/profileData.json', 'utf8');
-  const cv = JSON.parse(file);
-  return {
-    title: cv.general.displayName,
-    description: cv.general.byline || '',
-  };
+    const file = await fs.readFile(
+        process.cwd() + "/public/content/profileData.json",
+        "utf8",
+    );
+    const cv = JSON.parse(file);
+    return {
+        title: cv.general.displayName,
+        description: cv.general.byline || "",
+        metadataBase: new URL("https://taurean.work"),
+        openGraph: {
+            images: "/media/metadata-og-image.png",
+        },
+    };
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.variable} font-sans`}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.variable} font-sans`}>{children}</body>
+        </html>
+    );
 }
